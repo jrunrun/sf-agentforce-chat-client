2be41f69fb32396c292233949926e02a51161c8b
jrunrun <jpcraycraft@gmail.com>
2025-03-31 17:55:39 -0500

    https testing config

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
 // HTTP CONFIG
 
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
 
 
 
@@ -14,18 +14,19 @@ export default defineConfig({
 
 // FOR HTTPS CONFIG
 
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-// import fs from 'fs'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import fs from 'fs'
+import path from 'path'
 
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   server: {
-//     https: {
-//       key: fs.readFileSync('localhost+2-key.pem'),
-//       cert: fs.readFileSync('localhost+2.pem'),
-//     },
-//   },
-// })
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    https: {
+      key: fs.readFileSync(path.resolve(__dirname, 'https-testing/localhost+2-key.pem')),
+      cert: fs.readFileSync(path.resolve(__dirname, 'https-testing/localhost+2.pem')),
+    },
+  },
+})
 
