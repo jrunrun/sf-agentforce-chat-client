a7d85e38a99718acc6c7807401639366cf073da9
jrunrun <jpcraycraft@gmail.com>
2025-04-01 14:45:38 -0500

    railway config

--- vite.config.ts
+++ vite.config.ts
@@ -16,17 +16,13 @@
 
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import fs from 'fs'
-import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    https: {
-      key: fs.readFileSync(path.resolve(__dirname, 'https-testing/localhost+2-key.pem')),
-      cert: fs.readFileSync(path.resolve(__dirname, 'https-testing/localhost+2.pem')),
-    },
-  },
+    host: '0.0.0.0',
+    port: process.env.PORT ? parseInt(process.env.PORT) : 5173
+  }
 })
 
